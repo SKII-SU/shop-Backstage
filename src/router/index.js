c6ed04ae548c6badd51945ff0cfb4f0375bf8827
bigqianduan <bigqianduan@foxmail.com>
2019-04-22 16:07:16 +0800

    功能完善

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,28 @@ import login from '../components/login.vue'
 import Home from '../components/home.vue'
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
   routes: [
-    {path: '/', redirect: '/login'},
-    {path: '/login', component: login},
-    {path: '/home', component: Home}
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: login
+    },
+    {
+      path: '/home',
+      component: Home
+    }
   ]
 })
+
+router.beforeEach(function(to, from, next) {
+  if (window.sessionStorage.getItem('token') === null && to.path !== '/login') {
+    return next('/login')
+  }
+  next()
+})
+
+export default router
