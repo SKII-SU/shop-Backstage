5d55a8439f739c5ce8257c085aa9b85e471de0ec
bigqianduan <bigqianduan@foxmail.com>
2019-04-22 20:54:03 +0800

    侧边栏样式完成

--- src/main.js
+++ src/main.js
@@ -10,6 +10,19 @@ import axios from 'axios'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 axios.defaults.baseURL = 'http://127.0.0.1:11333/api/private/v1/'
+axios.interceptors.request.use(
+  function(config) {
+    // 给axios配置token
+    var token = window.sessionStorage.getItem('token')
+    if (token !== null) {
+      config.headers.Authorization = token
+    }
+    return config
+  },
+  function(error) {
+    return Promise.reject(error)
+  }
+)
 Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
